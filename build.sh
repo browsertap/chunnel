758b88ac30ed05e626ed5e8c79f97bb603009acb
Craig Condon <craig@spiceapps.com>
2012-12-20 16:20:43 -0600

    cleanup

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-rm -R ./dist/TapTunnel.app
+rm -rf ./TapTunnel
+mkdir TapTunnel;
 ./bundle.sh --name=TapTunnel --displayname="Tap Tunnel" --bundle-identifier=com.browsertap.taptunnel
+cp -R ./dist/TapTunnel.app ./TapTunnel;
+hdiutil create ./TapTunnel/TapTunnel.dmg -srcfolder ./TapTunnel -ov
+rm -rf ./dist
